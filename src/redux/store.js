5a6bb74e5650d39bda1b24081b40ca4b47ae6ef2
Yemesrach24 <99786617+Yemesrach24@users.noreply.github.com>
2025-04-10 23:50:42 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './userSlice';
-import messagesReducer from './messagesSlice';
+import messageReducer from './messageSlice';
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
-   messages: messagesReducer,
+    messages: messageReducer,
   },
 });
 
