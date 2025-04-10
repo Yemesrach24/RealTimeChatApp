2d7f610ad0cec961201203f47c4264ef094e190f
Yemesrach24 <99786617+Yemesrach24@users.noreply.github.com>
2025-04-10 23:44:55 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
-import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { store } from './redux/store';
+import './index.css';
 import App from './App';
-import Root from './index'; 
-
-
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <Root />
-  </React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter> 
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
-
-
