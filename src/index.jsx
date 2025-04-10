4d5477bc9bba0c6f9f53e5181090951fc9cdbe60
Yemesrach24 <99786617+Yemesrach24@users.noreply.github.com>
2025-04-10 23:46:29 +0300

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,1 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
-import App from './App';
 
-const Root = () => {
-  return (
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
-};
-
-export default Root;
