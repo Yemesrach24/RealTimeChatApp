3c6eab866ff07e5a63ba79b443ea9a0c4515aaf9
Yemesrach24 <99786617+Yemesrach24@users.noreply.github.com>
2025-04-11 00:11:06 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
-import LoginPage from './pages/LoginPage';
-import ChatPage from './pages/ChatPage';
-//import NotFoundPage from './pages/NotFoundPage';
-//import ChatRoom from './components/ChatRoom';
-import React, { useEffect } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';  
+import { useEffect } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { setUser } from './redux/userSlice';
 import { auth } from './firebase/config';
@@ -19,7 +13,6 @@ function App() {
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((firebaseUser) => {
       if (firebaseUser) {
-       
         dispatch(setUser({
           uid: firebaseUser.uid,
           email: firebaseUser.email,
@@ -27,38 +20,22 @@ function App() {
           photoURL: firebaseUser.photoURL,
         }));
       } else {
-       
         dispatch(setUser(null));
       }
     });
 
-    return () => unsubscribe(); 
+    return () => unsubscribe(); // Clean up on unmount
   }, [dispatch]);
 
   return (
     <Routes>
-   
+      {/* If the user is authenticated, redirect them to /chat */}
       <Route path="/" element={user ? <Navigate to="/chat" /> : <LoginPage />} />
+      
+      {/* If the user is authenticated, show the ChatPage, otherwise redirect to / */}
       <Route path="/chat" element={user ? <ChatPage /> : <Navigate to="/" />} />
     </Routes>
   );
 }
-
-export default App;
-
-
-const App = () => {
-  return (
-    <Router>
-      <div className="app">
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-           <Route path="/chat" element={<ChatPage />} /> 
-           {/* <Route path="*" element={<NotFoundPage />} />*/}
-        </Routes>
-      </div>
-    </Router>
-  );
-};
 
 export default App;
