113e4499daf7d036df6c97d1bbfec1bb913539aa
Yemesrach24 <99786617+Yemesrach24@users.noreply.github.com>
2025-04-10 23:47:13 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,47 @@ import LoginPage from './pages/LoginPage';
 import ChatPage from './pages/ChatPage';
 //import NotFoundPage from './pages/NotFoundPage';
 //import ChatRoom from './components/ChatRoom';
+import React, { useEffect } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';  
+import { useDispatch, useSelector } from 'react-redux';
+import { setUser } from './redux/userSlice';
+import { auth } from './firebase/config';
+import LoginPage from './pages/LoginPage';
+import ChatPage from './pages/ChatPage';
+
+function App() {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user.user);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((firebaseUser) => {
+      if (firebaseUser) {
+       
+        dispatch(setUser({
+          uid: firebaseUser.uid,
+          email: firebaseUser.email,
+          displayName: firebaseUser.displayName,
+          photoURL: firebaseUser.photoURL,
+        }));
+      } else {
+       
+        dispatch(setUser(null));
+      }
+    });
+
+    return () => unsubscribe(); 
+  }, [dispatch]);
+
+  return (
+    <Routes>
+   
+      <Route path="/" element={user ? <Navigate to="/chat" /> : <LoginPage />} />
+      <Route path="/chat" element={user ? <ChatPage /> : <Navigate to="/" />} />
+    </Routes>
+  );
+}
+
+export default App;
 
 
 const App = () => {
